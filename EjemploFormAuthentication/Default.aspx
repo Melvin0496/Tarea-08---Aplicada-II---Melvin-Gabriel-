caf0e101ca371fabfe9a26706b7e766bb4cd381a
Melvin0496 <melvingabriel65@gmail.com>
2016-02-09 10:11:26 -0400

    Ejemplo de webFormAuthentication

---
+++ EjemploFormAuthentication/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EjemploFormAuthentication.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<head>
+  <title>Forms Authentication - Default Page</title>
+</head>
+
+
+<body>
+  <h3>
+    Using Forms Authentication</h3>
+  <asp:Label ID="Welcome" runat="server" />
+  
+    <asp:Button ID="Submit1" OnClick="Signout_Click" 
+       Text="Sign Out" runat="server" /><p>
+  
+</body>
+
+</asp:Content>
---
+++ EjemploFormAuthentication/Logon.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace EjemploFormAuthentication
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Logon_Click(object sender, EventArgs e)
+        {
+            if ((UserEmail.Text == "MelvinGabriel@gmail.com") &&
+            (UserPass.Text == "1234"))
+            {
+                FormsAuthentication.RedirectFromLoginPage
+                   (UserEmail.Text, Persist.Checked);
+            }
+            else
+            {
+                Msg.Text = "Invalid credentials. Please try again.";
+            }
+        }
+    }
+}
